b553a6ecfe0c8bd6ee85160bb2218bdb2a1f320e
Samuel <samueleliasps4@gmail.com>
2025-03-27 22:50:31 -0300

    Desafios: Tabuada

--- Estudando.py
+++ Estudando.py
@@ -1,11 +1,32 @@
 ###################################### DESAFIOS #######################################
 
 
-#Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros.
+#Faça um programa que leia um numero inteiro qualquer e mostre na tela a sua tabuada.
 
-n1 = float(input('Digite uma medida em metros: '))
-cen = n1 * 100
-mili = n1 * 1000
-print(f'A medida em centimetros é {cen} e em milimetros é {mili}')
+print('TABUADA')
+
+n1 = int(input('Digite um numero: '))
+x1 = n1 * 1
+x2 = n1 * 2
+x3 = n1 * 3
+x4 = n1 * 4
+x5 = n1 * 5
+x6 = n1 * 6
+x7 = n1 * 7
+x8 = n1 * 8
+x9 = n1 * 9
+x10 = n1 * 10
+
+print('TABUADA DO: ')
+print(f'{n1} x {x1}')
+print(f'{n1} x {x2}')
+print(f'{n1} x {x3}')
+print(f'{n1} x {x4}')
+print(f'{n1} x {x5}')
+print(f'{n1} x {x6}')
+print(f'{n1} x {x7}')
+print(f'{n1} x {x8}')
+print(f'{n1} x {x9}')
+print(f'{n1} x {x10}')
 
 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Python.py
 Estudando Python
 
-Desafios curso em video: Este projeto é um programa simples em Python que converte medidas de metros para centímetros e milímetros. Ele é útil para tarefas cotidianas que envolvem conversão de unidades.
+Desafios curso em video: Este projeto é um programa simples em Python que exibe a tabuada do numero que o usuario digitou.
