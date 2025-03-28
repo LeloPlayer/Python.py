6dcf68df02a538811a74f38c44a7bf1ea90376dd
Samuel <samueleliasps4@gmail.com>
2025-03-27 22:52:45 -0300

    Desafios: Tabuada utilizando o loop "for"

--- Estudando.py
+++ Estudando.py
@@ -6,27 +6,10 @@
 print('TABUADA')
 
 n1 = int(input('Digite um numero: '))
-x1 = n1 * 1
-x2 = n1 * 2
-x3 = n1 * 3
-x4 = n1 * 4
-x5 = n1 * 5
-x6 = n1 * 6
-x7 = n1 * 7
-x8 = n1 * 8
-x9 = n1 * 9
-x10 = n1 * 10
 
-print('TABUADA DO: ')
-print(f'{n1} x {x1}')
-print(f'{n1} x {x2}')
-print(f'{n1} x {x3}')
-print(f'{n1} x {x4}')
-print(f'{n1} x {x5}')
-print(f'{n1} x {x6}')
-print(f'{n1} x {x7}')
-print(f'{n1} x {x8}')
-print(f'{n1} x {x9}')
-print(f'{n1} x {x10}')
+print(f'TABUADA DO {n1}:')
+
+for i in range(1, 11):
+    print(f'{n1} x {i} = {n1 * i}')
 
 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Python.py
 Estudando Python
 
-Desafios curso em video: Este projeto é um programa simples em Python que exibe a tabuada do numero que o usuario digitou.
+Desafios curso em video: Este projeto é um programa simples em Python que exibe a tabuada do numero que o usuario digitou, porem agora usando o loop "for".
