55b48dfab26e167293090cc8b619e9ad6a83b5f3
Samuel <samueleliasps4@gmail.com>
2025-03-27 22:25:59 -0300

    Operadores Aritmeticos

--- Estudando.py
+++ Estudando.py
@@ -1,3 +1,20 @@
-#inicio
+#OPERADORES ARITMÉTICOS
 
-print("Ola, mundo!!");
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+
+
+#Operador 'end' utilizado para acrescentar algo no final de uma linha.
+#Operador '\n' utilizado para pular uma linha em uma parte especifica do codigo.
+#Operador ' : ' utilizado para escrever os espaços, podemos adicionar a quantidade de espaço que queremos no codigo, ':30' da 30 linhas de espaço.
+#Operador '> Direita' e '< Esquerda' utilizado para alinhamento de caracteres, utilizamos tambem ' ^ centro ' para fazer a centralizaçao dos caracteres.
+
+print(f'A soma é: {s}, \n o produto é {m}, \n e a divisão é {d:.3}', end = ' >>> ' ) 
+print(f'A divisão inteira é {di} \n a potençia é {e}')
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Python.py
 Estudando Python
+
+Operadores aritimeticos: Nesse codigo o usuario digita um valor, logo digita outro valor, e em seguida é exibido a soma, produto, divisao e potencia dos valores
