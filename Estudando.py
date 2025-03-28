8de41634d2c3c4d4eff397aff81ddf5aa7f1d933
Samuel <samueleliasps4@gmail.com>
2025-03-27 22:31:25 -0300

    Desafios: Sucessor e antecessor

--- Estudando.py
+++ Estudando.py
@@ -1,20 +1,9 @@
-#OPERADORES ARITMÉTICOS
+###################################### DESAFIOS #######################################
 
 
-n1 = int(input('Digite um valor: '))
-n2 = int(input('Digite outro valor: '))
+#Faça um programa que leia um numero inteiro e mostre na tela o seu sucessor e seu antecessor.
 
-s = n1 + n2
-m = n1 * n2
-d = n1 / n2
-di = n1 // n2
-e = n1 ** n2
-
-
-#Operador 'end' utilizado para acrescentar algo no final de uma linha.
-#Operador '\n' utilizado para pular uma linha em uma parte especifica do codigo.
-#Operador ' : ' utilizado para escrever os espaços, podemos adicionar a quantidade de espaço que queremos no codigo, ':30' da 30 linhas de espaço.
-#Operador '> Direita' e '< Esquerda' utilizado para alinhamento de caracteres, utilizamos tambem ' ^ centro ' para fazer a centralizaçao dos caracteres.
-
-print(f'A soma é: {s}, \n o produto é {m}, \n e a divisão é {d:.3}', end = ' >>> ' ) 
-print(f'A divisão inteira é {di} \n a potençia é {e}')
+n1 = int(input('Digite um numero: '))
+suc = n1 + 1
+ant = n1 - 1
+print(f'O sucessor de {n1} é {suc}, eo antecessor é {ant}')
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Python.py
 Estudando Python
 
-Operadores aritimeticos: Nesse codigo o usuario digita um valor, logo digita outro valor, e em seguida é exibido a soma, produto, divisao e potencia dos valores
+Desafios curso em video: Nesse codigo apresento o inicio dos desafios fornecidos no curso em video de python pelo professor gustavo guanabara, aonde o usuario digita um valor
+logo em seguida é exibido o numero seguinte eo numero anterior no qual o usuario digitou.
