0487dba7fa427eec29d79e74754e3c58dcd37123
Samuel <samueleliasps4@gmail.com>
2025-03-31 18:47:21 -0300

    Desafios: Quebrando um número 2.0

--- Estudando.py
+++ Estudando.py
@@ -3,14 +3,15 @@
 # Crie um programa que leia um numero Real qualquer pelo teclado e mostre na tela sua porçao inteira
 # Exemplo: Digite 6.127 o numero tem que apareçer a parte inteira 6.
 
-from math import trunc
+#from math import trunc
 
-num = float(input('Digite um numero Real: '))
-print('A parte inteira do numero {} é {}'.format(num, trunc(num)))
-
-
+#num = float(input('Digite um numero Real: '))
+#print('A parte inteira do numero {} é {}'.format(num, trunc(num)))
 
 
+
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a o seu valor inteiro é igual a {}'.format(num, int(num)))
 
 
 
--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Estudando Python
 
 Desafios curso em video:
 
-Quebrando um numero: Nesse codigo o usuario digita o numero Real, e logo em seguida é exibido o numero inteiro do valor digitado pelo usuario.
+Quebrando um numero: Nesse codigo o usuario digita o numero Real, e logo em seguida é exibido o numero inteiro do valor digitado pelo usuario. Dessa vez sem a importaçao da biblioteca math, apenas utilizando a bibliotexa padrao python.
