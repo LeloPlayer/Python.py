eff0550445419ed42d8af39bf452324602652b6a
Samuel <samueleliasps4@gmail.com>
2025-04-03 19:38:56 -0300

    Desafios: Tocando musica 🎵

--- Estudando.py
+++ Estudando.py
@@ -1,28 +1,14 @@
 #############################################################################
 
-#Strings, frases e operadores textuais: testes!!
 
-frase = 'Ola mundo python'
-print(frase[2:10]) #Do segundo caractere até o 10 caractere, lembrando que a contagem começam pelo 0.
-print(frase.upper().count('O')) #Quantas vezes a letra 'O' se repete na frase
-print(len(frase)) #Quantos caracteres tem na frase
-print(frase.replace('Python', 'Android')) #Substitui a palavra python por android na frase
-print(frase) #Exibe a frase
-print ('Ola' in frase) #Verifica se há a palavra "Ola"  na frase, se sim sera exibido TRUE se nao será exibido FALSE.
-print (frase.find('mundo')) #Encontra a palavra mundo
-print(frase.split()) #Divide a frase por partes
-dividido = frase.split() #Variavel com a frase dividida
-print(dividido[2]) #Variavel exibindo a segunda palavra da frase
-print(dividido[2][3]) #Variavel exibndo a segunda palavra da frase e o 3 caractere dessa palavra.
+#Tocando musica!!!
 
+import pygame
 
-print('Ola')
+pygame.init()
 
-print("""Between the chaos of the days and the calm of the nights, 
-he found refuge in the little details: the smell of fresh coffee,
-the sound of the rain hitting the window and the silent glow of the stars that 
-seemed to understand his deepest thoughts. Sometimes, all 
-sometimes all you need is a moment of peace to realize that life is still 
-dancing, even when you stop to breathe.""")
+pygame.mixer.music.load('Swedish House Mafia and The Weeknd - Moth To A Flame (Official Video).mp3')
+
+pygame.mixer.music
 
 
--- README.md
+++ README.md
@@ -1,12 +1,9 @@
 # Python.py
 Estudando Python
 
-Neste codigo utilizo alguns operadores de texto, str, que vao servir para manipular frases, textos e caracteres. Nesse codigo utilizei alguns dos operadores abaixo que estou aprendendo nesse modulo.
 
-<<<<<<< HEAD
-Operações com String no Python:As principais operações que estou aprendendo nesse módulo são o Fatiamento de String, Análise com len(), count(), find(), transformações com replace(), upper(), lower(), capitalize(), title(), strip(), junção com join().
-=======
 Tocando uma musica??: Neste codigo utilizamos a biblioteca do pygame, utilizada para criaçao de games... para usar essa biblioteca é necessario instalar ela em seu dispositivo. O algoritimo toca a musica "Moth to a flame - The Weeknd". 
 
 Coloque o fone e aperte o play!!
->>>>>>> 3a14ade570e1b4c68557f1dcee278612ce8120eb
+
+
